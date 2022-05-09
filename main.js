285b22efd7fc6fde57ce3defe4e93118259cdbeb
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-05-09 13:40:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
 function setup(){
     canvas=createCanvas(280,280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
 function clearCanvas(){
     background("white");
 }
-    
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label:'+results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence:'+ Math.round(results[0].confidence*100)+'%';
+
+    utterthis = new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterthis);
+}
+  
